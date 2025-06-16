53a381961a29bc8ca90477afc9477e9de6464a86
adwaith5902 <adwaithchandran2095@gmail.com>
2025-06-17 05:24:34 +0530

    Delete Sales_6.sql

--- Sales_6.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE sales_dataset.State_Records AS
-SELECT 
-DISTINCT ssdv.State AS State ,
-SUM(ssdv.Profit ) AS Profit
-FROM sales_dataset.superstores_sales_data_v1 ssdv
-GROUP BY State
-ORDER BY Profit DESC 
